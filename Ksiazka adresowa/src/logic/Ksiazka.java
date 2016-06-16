8ebc32f91198c28098cba3f7fe1293b766e1aa07
jedras55 <jedrzej.ostrowski@gmail.com>
2016-06-17 01:31:02 +0200

    Wygenerowałem klasę kontrolera - Ksiazka.java

--- Ksiazka adresowa/src/logic/Ksiazka.java
+++ Ksiazka adresowa/src/logic/Ksiazka.java
@@ -1,5 +1,32 @@
 package logic;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Ksiazka {
+
+    @FXML
+    private TextField textFieldMiasto;
+
+    @FXML
+    private TextField textFieldNumerDomu;
+
+    @FXML
+    private TextField textFieldNumerTelefonu;
+
+    @FXML
+    private TextField textFieldEmail;
+
+    @FXML
+    private TextField textFieldUlica;
+
+    @FXML
+    private TextField textFieldImie;
+
+    @FXML
+    private TextField textFieldNazwisko;
+
+    @FXML
+    private TextField textFieldKodPocztowy;
 
 }
