41a77ad7f3d2e390ec355ee0a589d1ac197c7ea0
jedras55 <jedrzej.ostrowski@gmail.com>
2016-06-17 17:10:02 +0200

    Dodałem dodawanie, edycje i usuwanie użytkowników

--- Ksiazka adresowa/src/logic/Ksiazka.java
+++ Ksiazka adresowa/src/logic/Ksiazka.java
@@ -1,11 +1,9 @@
 package logic;
 
-import java.util.Observable;
-
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -39,8 +37,32 @@ public class Ksiazka {
     @FXML
     private TextField textFieldKodPocztowy;
 
+    @FXML
+    private Button buttonUsun;
+
+    @FXML
+    private Button buttonNowy;
+
+    @FXML
+    private Button buttonEdytuj;
+
+    @FXML
+    private Button buttonAnuluj;
+
+    @FXML
+    private Button buttonAktualizuj;
+
+    @FXML
+    private Button buttonDodaj;
+
+    @FXML
+    private Button buttonDodajAnuluj;
+
+    public Osoba wybrana = null;
+
     public void wybierzOsobe(){
-    	Osoba wybrana = listViewKontakty.getSelectionModel().getSelectedItem();
+    	wybrana = listViewKontakty.getSelectionModel().getSelectedItem();
+    	wyjdzZTrybuEdycji();
 
     	textFieldImie.setText(wybrana.getImie());
     	textFieldNazwisko.setText(wybrana.getNazwisko());
@@ -53,11 +75,74 @@ public class Ksiazka {
     	zablokujTextFieldy();
     }
 
-    public void testy(){
+    public void dodajTestoweWartosci(){
     	osoby.add(new Osoba("Jedrzej", "Ostrowski", "531363458", "jedrzej.ostrowski@gmail.com", "Milejewo", "Pomorska Wies", "7", "82-316"));
     	osoby.add(new Osoba("Marek", "Nowak", "663490220", "marek.nowak@gmail.com", "Elbl퉓", "Grunwaldzka", "102", "82-300"));
+    	osoby.add(new Osoba("Micha", "Wa퓆iak", "663490220", "marek.nowak@gmail.com", "Elbl퉓", "Grunwaldzka", "102", "82-300"));
+    	osoby.add(new Osoba("Pawel", "Mezenski", "663490220", "marek.nowak@gmail.com", "Elbl퉓", "Grunwaldzka", "102", "82-300"));
+    	osoby.add(new Osoba("Adam", "Malysz", "663490220", "marek.nowak@gmail.com", "Elbl퉓", "Grunwaldzka", "102", "82-300"));
+    	osoby.add(new Osoba("Robert", "Kubica", "663490220", "marek.nowak@gmail.com", "Elbl퉓", "Grunwaldzka", "102", "82-300"));
+
     	listViewKontakty.setItems(osoby);
     }
+
+    public void Nowy(){
+    	przejdzWTrybDodawania();
+    	wyczyscTextFieldy();
+    }
+
+    public void Edytuj(){
+    	if(wybrana != null){
+    		przejdzWTrybEdycji();
+    	}
+    	else{
+
+    	}
+    }
+
+    public void Usun(){
+    	if(wybrana != null){
+    		osoby.remove(wybrana);
+    		wyczyscTextFieldy();
+    		listViewKontakty.getSelectionModel().clearSelection();
+    	}
+    }
+    public void Dodaj(){
+    	wyjdzZTrybuDodawania();
+
+    	String imie = textFieldImie.getText();
+    	String nazwisko = textFieldNazwisko.getText();
+    	String numerTelefonu = textFieldNumerTelefonu.getText();
+    	String email = textFieldEmail.getText();
+    	String miasto = textFieldMiasto.getText();
+    	String ulica = textFieldUlica.getText();
+    	String numerDomu = textFieldNumerDomu.getText();
+    	String kodPocztowy = textFieldKodPocztowy.getText();
+
+    	Osoba nowa = new Osoba(imie, nazwisko, numerTelefonu, email, miasto, ulica, numerDomu, kodPocztowy);
+    	osoby.add(nowa);
+    	listViewKontakty.setItems(osoby);
+    }
+    public void DodajAnuluj(){
+    	wyjdzZTrybuDodawania();
+    }
+
+    public void Aktualizuj(){
+    	wybrana.setImie(textFieldImie.getText());
+    	wybrana.setNazwisko(textFieldNazwisko.getText());
+    	wybrana.setNumerTelefonu(textFieldNumerTelefonu.getText());
+    	wybrana.setEmail(textFieldEmail.getText());
+    	wybrana.setMiasto(textFieldMiasto.getText());
+    	wybrana.setUlica(textFieldUlica.getText());
+    	wybrana.setNumerDomu(textFieldNumerDomu.getText());
+    	wybrana.setKodPocztowy(textFieldKodPocztowy.getText());
+    	wyjdzZTrybuEdycji();
+    }
+
+    public void Anuluj(){
+    	wyjdzZTrybuEdycji();
+    }
+
     public void zablokujTextFieldy(){
     	textFieldImie.setEditable(false);
     	textFieldNazwisko.setEditable(false);
@@ -77,5 +162,47 @@ public class Ksiazka {
     	textFieldUlica.setEditable(true);
     	textFieldNumerDomu.setEditable(true);
     	textFieldKodPocztowy.setEditable(true);
+    }
+    public void przejdzWTrybDodawania(){
+    	buttonNowy.setVisible(false);
+    	buttonEdytuj.setVisible(false);
+    	buttonUsun.setVisible(false);
+    	buttonDodaj.setVisible(true);
+    	buttonDodajAnuluj.setVisible(true);
+    	odblokujTextFieldy();
+    }
+    public void wyjdzZTrybuDodawania(){
+    	buttonNowy.setVisible(true);
+    	buttonEdytuj.setVisible(true);
+    	buttonUsun.setVisible(true);
+    	buttonDodaj.setVisible(false);
+    	buttonDodajAnuluj.setVisible(false);
+    	zablokujTextFieldy();
+    }
+    public void przejdzWTrybEdycji(){
+    	buttonNowy.setVisible(false);
+    	buttonEdytuj.setVisible(false);
+    	buttonUsun.setVisible(false);
+    	buttonAktualizuj.setVisible(true);
+    	buttonAnuluj.setVisible(true);
+    	odblokujTextFieldy();
+    }
+    public void wyjdzZTrybuEdycji(){
+    	buttonNowy.setVisible(true);
+    	buttonEdytuj.setVisible(true);
+    	buttonUsun.setVisible(true);
+    	buttonAktualizuj.setVisible(false);
+    	buttonAnuluj.setVisible(false);
+    	zablokujTextFieldy();
+    }
+    public void wyczyscTextFieldy(){
+    	textFieldImie.setText("");
+    	textFieldNazwisko.setText("");
+    	textFieldNumerTelefonu.setText("");
+    	textFieldEmail.setText("");
+    	textFieldMiasto.setText("");
+    	textFieldUlica.setText("");
+    	textFieldNumerDomu.setText("");
+    	textFieldKodPocztowy.setText("");
     }
 }
