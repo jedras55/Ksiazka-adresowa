2b3b02fa05bfce8700aca65701e2bbe814d9c561
jedras55 <jedrzej.ostrowski@gmail.com>
2016-06-20 23:55:13 +0200

    Usunąłem niepotrzebne fx:id, dodałem polską informacje dla pustej książki

--- Ksiazka adresowa/src/logic/Ksiazka.java
+++ Ksiazka adresowa/src/logic/Ksiazka.java
@@ -2,21 +2,53 @@ package logic;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class Ksiazka {
 
     @FXML
-    void Importuj(ActionEvent event) {
+    private TableColumn<?, ?> tableColumnNazwisko;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnNumerTelefonu;
+
+    @FXML
+    private TableView<?> tableViewKsiazkaAdresowa;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnImie;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnEmail;
+
+    @FXML
+    void nowy(ActionEvent event) {
 
     }
 
     @FXML
-    void Eksportuj(ActionEvent event) {
+    void otworz(ActionEvent event) {
 
     }
 
     @FXML
-    void Zamknij(ActionEvent event) {
+    void zapisz(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dodaj(ActionEvent event) {
+
+    }
+
+    @FXML
+    void edytuj(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usun(ActionEvent event) {
 
     }
 
